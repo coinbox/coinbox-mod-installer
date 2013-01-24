1d8790af87b29c70471852af0d1f4b3c9a58c2a7
jadkik <jadkik94@gmail.com>
2013-01-24 04:33:01 +0200

    Changed menu according to changes in core

--- cbpos/mod/installer/__init__.py
+++ cbpos/mod/installer/__init__.py
@@ -6,17 +6,24 @@ class ModuleLoader(BaseModuleLoader):
     name = 'Module Installer and Manager'
 
     def menu(self):
+        from cbpos.interface import MenuItem
         from cbpos.mod.installer.views import ModulesPage
             
         return [[],
-                [{'parent': 'System', 'label': 'Modules', 'page': ModulesPage, 'image': cbpos.res.installer('images/menu-modules.png')}]]
+                [MenuItem('modules', parent='system',
+                          label=cbpos.tr.installer._('Modules'),
+                          icon=cbpos.res.installer('images/menu-modules.png'),
+                          page=ModulesPage
+                          )
+                 ]
+                ]
 
     def argparser(self):
         parser1 = cbpos.subparsers.add_parser('modules', description="Modules operations")
         parser1.set_defaults(handle=self.arg_handler)
 
     def init(self):
-        # Check for updates
+        # TODO: Check for updates
         return True
 
     def arg_handler(self, args):
